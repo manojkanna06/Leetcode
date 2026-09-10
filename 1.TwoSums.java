99583b06abef4b9fbd6cf0c0ab8f04abffef06df
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-10 09:38:47 +0530

    Create 1.TwoSums.java

---
+++ 1.TwoSums.java
@@ -0,0 +1,15 @@
+//TC O(n) SC O(n)
+//1.Two sums
+//brute force
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int sum=nums[i]+nums[j];
+if(sum==target){
+return new int[]{i,j};
+            }
+            }
+    }return new int[]{};
+}
+}
