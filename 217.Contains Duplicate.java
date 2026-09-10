5433a9bfba15df5c10356adf28d36ce25e7f27dc
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-10 09:38:00 +0530

    Create 217.Contains Duplicate.java

---
+++ 217.Contains Duplicate.java
@@ -0,0 +1,28 @@
+//217.Contains Duplicate
+//Brute fore
+class Solution {
+    public boolean containsDuplicate(int[] nums) { 
+        for(int i=0;i<=nums.length-1;i++){
+            for(int key=i+1;key<nums.length;key++){
+                if(nums[i]==nums[key]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+//contain TC O(n^2) SC O(1) ,execution time may exceeds,so use heapset
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
