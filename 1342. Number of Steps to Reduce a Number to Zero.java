ebd1bf9729361d6e2c12a6c88d3f9f8d23d70e1b
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-25 06:02:44 +0530

    Update 1342. Number of Steps to Reduce a Number to Zero.java

--- 1342. Number of Steps to Reduce a Number to Zero.java
+++ 1342. Number of Steps to Reduce a Number to Zero.java
@@ -2,7 +2,7 @@ class Solution {
     public int numberOfSteps(int num) {
         int count=0;
 
-        while(num!=0){
+        while(num!=0){ //for(;num!=0;){
 
             if(num%2==0){
                 num=num/2;
@@ -18,7 +18,8 @@ class Solution {
         return count;
     }
 }
-/* error 
+//TC O(log n)  SC O(1)
+/*PF: error 
   class Solution {
     public int numberOfSteps(int num) {
         int count=0;
@@ -41,4 +42,19 @@ class Solution {
 error:compile error due to no return statement 
   as numberOfSteps(method) declared as int ,it should return int
   println() only prints the value. It does not return it.
+  for-each loop, then you cannot directly use it for this problem.
+
+A Java for-each loop is used to go through elements of an array/collection:
+
+for (int x : arr) {
+    System.out.println(x);
+}
+numberOfSteps(int num)
+
+you have one integer, not an array or collection.
+
+So this is not possible:
+
+for (int x : num)   
+num is not iterable
   */
